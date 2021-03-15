fbda62e2f418d64c4d811af352d7b0d6e58972eb
pico <pivillaa@informatik.hu-berlin.de>
2021-03-15 02:13:31 +0100

    added all main functions

--- my_gym/my_gym/envs/gatekeeper/gatekeeper_env.py
+++ my_gym/my_gym/envs/gatekeeper/gatekeeper_env.py
@@ -8,22 +8,28 @@ import random
 #import glob
 #import re
 
-MAP = ["--.G","---.","-++-","----"]
+MAP =  ["--.G",
+        "---.",
+        "-++-",
+        "----"]
 
 LEFT = 0
 DOWN = 1
 RIGHT = 2
 UP = 3
 
-GATEKEEPER_TYPES = ['L','D','R','U']
+GATEKEEPER_TYPES = ['L','D','R','U'] #idx is equivalent to action required by the gatekeeper type ('L'=0=LEFT,'R'=2=RIGHT)
 
-CELL_IMAGES = {"-":np.array([[255,255],[255,255]]), "L":np.array([[0,255],[0,255]]), "D":np.array([[255,255],[0,0]]), "R":np.array([[255,0],[255,0]]), "U":np.array([[0,0],[255,255]]),   }
+GATEKEEPER_LOCATIONS = [(0,2),(1,3)]
+
+CELL_IMAGES = {"-":np.array([[255,255],[255,255]]), "L":np.array([[0,255],[0,255]]), "D":np.array([[255,255],[0,0]]), "R":np.array([[255,0],[255,0]]), "U":np.array([[0,0],[255,255]]), "G":np.array([[0,0],[0,0]])   }
 
 class GatekeeperEnv(gym.Env):
     def __init__(self):#, params):
         super(GatekeeperEnv, self).__init__()
 
         self.max_steps = 100
+        self.step_count = 0
 
         self.action_space = spaces.Discrete(4)
         self.position_ob_space  =  spaces.Discrete(16)
@@ -35,14 +41,40 @@ class GatekeeperEnv(gym.Env):
         
     
     def reset(self):
-        self.curr_loc = [3,1] #staring position
+        self.done = False
+        self.step_count = 0
+        self.curr_loc =  (3,1) #staring position
         self.map = np.asarray(MAP, dtype='c')
         self.insert_rnd_gatekeepers() #select randomly two gatekeepers from 4 possible types
-
-        return ( CELL_IMAGES['-'] , self.vec2n(self.curr_loc))
-     
+        self.curr_img = CELL_IMAGES['-']
+        return (self.curr_img, self.vec2n(self.curr_loc))
+      
     def step(self, action):
-        return 0, 0, 0, {}
+        if self.curr_loc in  GATEKEEPER_LOCATIONS:
+            #if agent is located in gatekeeper, check if the executed action is the one asked
+            if self.is_right_action(self.map[self.curr_loc[0],self.curr_loc[1]].decode('UTF-8'),action):
+                reward=10 #goal achieved
+                self.curr_loc= (0,3)
+                self.curr_img = CELL_IMAGES['G']
+            else: #wrong password, agent loses
+                reward= -10
+                self.curr_img = CELL_IMAGES[self.map[self.curr_loc[0],self.curr_loc[1]].decode('UTF-8')]
+            self.done = True
+        
+        else:
+            reward = -1
+            new_loc = self.inc(self.curr_loc[0], self.curr_loc[1], action)
+            #if agent moves over a block, return to previous position
+            if self.map[new_loc[0],new_loc[1]] != b'+':
+                self.curr_loc = new_loc
+            self.curr_img = CELL_IMAGES[self.map[self.curr_loc[0],self.curr_loc[1]].decode('UTF-8')]
+
+        #print(self.curr_loc )
+        self.step_count+= 1
+        if self.step_count >= self.max_steps:
+            self.done =True
+
+        return (self.curr_img, self.vec2n(self.curr_loc)), reward, self.done, {}
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
@@ -50,24 +82,26 @@ class GatekeeperEnv(gym.Env):
 
 
     def insert_rnd_gatekeepers(self):
-        gk = random.sample(GATEKEEPER_TYPES, 2)
-        self.map[0,2] = gk[0]
-        self.map[1,3] = gk[1]
+        gk = random.sample(GATEKEEPER_TYPES, len(GATEKEEPER_LOCATIONS))
+        for l,k in zip(GATEKEEPER_LOCATIONS,gk):
+            self.map[l[0],l[1]] = k
 
-    def vec2n(vec):
-        return vec[0]*self.maps.shape[1] + vec[1]
+    def vec2n(self,vec):
+        return vec[0]*self.map.shape[1] + vec[1]
 
     def inc(self,row, col, a):
         if a == LEFT:
-            ncol = max(col - 1, 0)
+            col = max(col - 1, 0)
         elif a == DOWN:
-            nrow = min(row + 1, self.map.shape[0] - 1)
+            row = min(row + 1, self.map.shape[0] - 1)
         elif a == RIGHT:
-            ncol = min(col + 1, self.map.shape[1] - 1)
+            col = min(col + 1, self.map.shape[1] - 1)
         elif a == UP:
-            nrow = max(row - 1, 0)
-        
-        #if agent moves over a block, return to previous position
-        if self.map[nrow,ncol] == '+':
-            return (row,col)
-        return (nrow, ncol)
+            row = max(row - 1, 0) 
+        return row, col
+
+    def is_right_action(self,pwd,action):
+        if GATEKEEPER_TYPES[action] == pwd:
+            return True
+        else:
+            return False
